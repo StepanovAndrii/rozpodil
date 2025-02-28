ffb315d2e18dabb93bdb3983f90b6d09cd9f53c2
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-02-28 20:26:24 +0200

    chore(backend): configure CORS policy for localhost:4200

---
+++ backend/Configurations/CorsConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace backend.Configurations
+{
+    public static class CorsConfig
+    {
+        public static IServiceCollection AddCustomCors(this IServiceCollection services)
+        {
+            var servicesProvider = services.BuildServiceProvider();
+            var configuration = servicesProvider.GetRequiredService<IConfiguration>();
+
+            var allowedOrigins = configuration.GetSection("AllowedOrigins").Get<string[]>() ?? [];
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost4200", policy =>
+                    policy.WithOrigins(allowedOrigins)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                );
+            });
+
+            return services;
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,5 @@
+using backend.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCustomCors();
 
 var app = builder.Build();
 
@@ -15,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowLocalhost4200");
 
 app.UseHttpsRedirection();
 
