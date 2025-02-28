da56acde62ab274592f50a1f23b55b8d46a69697
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-03-01 00:22:27 +0200

    feat(frontend): add authorization service

---
+++ frontend/src/app/services/authentication/auth.service.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { AuthConfigService } from './auth-config/auth-config.service';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  private _authService: OAuthService = inject(OAuthService);
+  private _configuration: AuthConfigService = inject(AuthConfigService);
+  
+  constructor() { }
+
+  public loginWithProvider(issuer: string, clientId: string) {
+    const googleAuthConfig: AuthConfig = this._configuration.getAuthConfig(
+      issuer,
+      clientId
+    );
+
+    this._authService.configure(googleAuthConfig);
+    this._authService.loadDiscoveryDocument();
+    this._authService.initCodeFlow();
+  }
+}
