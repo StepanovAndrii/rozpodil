951e9cc8d47c27577a4f4c078d3e6ed059464339
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-03-01 00:51:58 +0200

    chore(frontend): create empty page for login callbacks

---
+++ frontend/src/app/pages/callback-page/callback-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-callback-page',
+  imports: [],
+  templateUrl: './callback-page.component.html',
+  styleUrl: './callback-page.component.scss'
+})
+export class CallbackPageComponent {
+
+}
