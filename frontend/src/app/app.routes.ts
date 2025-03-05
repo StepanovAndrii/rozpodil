055539acd1a5e582a506978df7bede181a57d6a4
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-03-06 01:22:11 +0200

    feat(auth/router): configure routes for login and callback

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './features/authentication/login-page/login-page.component';
+import { LoginCallbackPageComponent } from './features/authentication/login-callback-page/login-callback-page.component';
 
 export const routes: Routes = [
-    
+    {path: '', redirectTo: 'auth', pathMatch: 'full'},
+    {path: 'auth', component: LoginPageComponent},
+    {path: 'auth/callback', component: LoginCallbackPageComponent}
 ];
