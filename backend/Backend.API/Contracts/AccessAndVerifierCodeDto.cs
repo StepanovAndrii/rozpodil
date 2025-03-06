8ce46a3bd76159bf0cf52dc0d26c5f24655923cc
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-03-06 16:16:58 +0200

    feat(Backend.API/Contracts): create dto for access code and verifier code

---
+++ backend/Backend.API/Contracts/AccessAndVerifierCodeDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Backend.API.Contracts
+{
+    public record AccessAndVerifierCodeDto
+    {
+        [JsonPropertyName("code")]
+        public string code { get; init; } = string.Empty;
+        [JsonPropertyName("code_verifier")]
+        public string codeVerifier { get; init; } = string.Empty;
+    }
+}
