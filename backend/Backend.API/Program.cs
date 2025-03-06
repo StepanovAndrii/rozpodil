84191a06e9d1382e836da755cd05bcd3e547b18a
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-03-06 16:41:53 +0200

    feat(backend/config): add cors config

--- backend/Backend.API/Program.cs
+++ backend/Backend.API/Program.cs
@@ -1,3 +1,5 @@
+using Backend.API.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCustomCors();
 
 var app = builder.Build();
 
@@ -16,10 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowLocalhost4200");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
