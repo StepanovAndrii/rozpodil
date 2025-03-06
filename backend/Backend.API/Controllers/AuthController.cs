f5df58cff54ca546e944814ca1ad94acb5d7a223
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-03-06 16:43:01 +0200

    feat(backend): create auth controller

---
+++ backend/Backend.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Backend.API.Contracts;
+using Backend.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            this._authService = authService;
+        }
+
+        [HttpPost("exchange-code")]
+        public async ActionResult ExchangeAuthorizationCode([FromBody] AccessAndVerifierCodeDto request)
+        {
+            Response.Cookies.Append("user_token", _authService.TryGetToken(request.code, request.codeVerifier);)
+            return Ok();
+        }
+    }
+}
