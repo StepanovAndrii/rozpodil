268000fff06d46626e157a1da221bce600632790
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-01 20:16:07 +0300

    feat(frontend): create room pages

---
+++ frontend/src/app/features/room/room-creation/room-creation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-creation',
+  imports: [],
+  templateUrl: './room-creation.component.html',
+  styleUrl: './room-creation.component.scss'
+})
+export class RoomCreationComponent {
+
+}
---
+++ frontend/src/app/features/room/room-joining/room-joining.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-joining',
+  imports: [],
+  templateUrl: './room-joining.component.html',
+  styleUrl: './room-joining.component.scss'
+})
+export class RoomJoiningComponent {
+
+}
