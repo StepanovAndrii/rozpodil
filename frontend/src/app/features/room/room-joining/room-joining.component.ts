6cc6f67f82540c6c2268307411cdebf5f07b7910
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-02 01:29:00 +0300

    feat(room-creation): create room joining page

--- frontend/src/app/features/room/room-joining/room-joining.component.ts
+++ frontend/src/app/features/room/room-joining/room-joining.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { AccessControlService } from '../../../core/services/access-control-service/access-control.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-room-joining',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './room-joining.component.html',
   styleUrl: './room-joining.component.scss'
 })
 export class RoomJoiningComponent {
+  constructor(
+    private accessControlService: AccessControlService
+  ) { }
 
+  public allowAccess() : void {
+    this.accessControlService.enable();
+  }
 }
