fadd5cc37100a519ca4bceff9e87212d5c75b2d6
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-02 01:28:41 +0300

    feat(room-creation): create room creation page

--- frontend/src/app/features/room/room-creation/room-creation.component.ts
+++ frontend/src/app/features/room/room-creation/room-creation.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { AccessControlService } from '../../../core/services/access-control-service/access-control.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-room-creation',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './room-creation.component.html',
   styleUrl: './room-creation.component.scss'
 })
-export class RoomCreationComponent {
 
+export class RoomCreationComponent {
+  constructor(
+    private accessControlService: AccessControlService
+  ) { }
+
+  public allowAccess() : void {
+    this.accessControlService.enable();
+  }
 }
