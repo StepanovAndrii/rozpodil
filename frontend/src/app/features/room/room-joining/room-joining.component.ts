d893de538dae657f78744d752a3b2ed3a610caf3
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-02 13:06:55 +0300

    refactor(room-joining): change routing behaviour

--- frontend/src/app/features/room/room-joining/room-joining.component.ts
+++ frontend/src/app/features/room/room-joining/room-joining.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { AccessControlService } from '../../../core/services/access-control-service/access-control.service';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room-joining',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './room-joining.component.html',
   styleUrl: './room-joining.component.scss'
 })
 export class RoomJoiningComponent {
   constructor(
-    private accessControlService: AccessControlService
+    private accessControlService: AccessControlService,
+    private router: Router
   ) { }
 
-  public allowAccess() : void {
+  public changeToCreate() : void {
     this.accessControlService.enable();
+this.router.navigate(['/room/create'], { replaceUrl: true })
   }
 }
