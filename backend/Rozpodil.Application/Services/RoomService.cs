9e10115a5c65ca80249799c5dea9157539873db4
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-04 11:10:24 +0300

    feat(backend): create room service

---
+++ backend/Rozpodil.Application/Services/RoomService.cs
@@ -0,0 +1,20 @@
+﻿using Rozpodil.Domain.Repositories;
+
+namespace Rozpodil.Application.Services
+{
+    public class RoomService
+    {
+        private readonly IRoomRepository _repository;
+        public RoomService(
+            IRoomRepository repository
+            )
+        {
+            _repository = repository;
+        }
+
+        public async void GetRoomsByUserId(Guid userId)
+        {
+            await _repository.GetRoomsByUserIdAsync(userId);
+        }
+    }
+}
