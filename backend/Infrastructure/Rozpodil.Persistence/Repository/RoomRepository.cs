3298cf04225ed887c42034296360092583872e0d
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-04 03:18:05 +0300

    feat(backend): create room and user repositories

---
+++ backend/Infrastructure/Rozpodil.Persistence/Repository/RoomRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rozpodil.Domain.Entities;
+
+namespace Rozpodil.Persistence.Repository
+{
+    public class RoomRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public RoomRepository(
+            DatabaseContext context
+            ) {
+            _context = context;
+        }
+
+        public async Task CreateRoom(Room room)
+        {
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Room>> GetRoomsByUserIdAsync(Guid userId)
+        {
+            return await _context.Rooms
+                .Where(room => room.Users.Any(user => user.Id == userId))
+                .ToListAsync();
+        }
+    }
+}
---
+++ backend/Infrastructure/Rozpodil.Persistence/Repository/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rozpodil.Domain.Entities;
+
+namespace Rozpodil.Persistence.Repository
+{
+    public class UserRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public UserRepository(
+            DatabaseContext context
+            ) {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserById(Guid id)
+        {
+            return await _context.Users
+                .Where(user => user.Id == id)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
