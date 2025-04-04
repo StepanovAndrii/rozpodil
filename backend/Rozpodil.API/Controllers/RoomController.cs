f50029d3481ebb9f2dcbdcd6b8297884fd49a88b
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-04 11:12:04 +0300

    feat(backend): create room controller

---
+++ backend/Rozpodil.API/Controllers/RoomController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rozpodil.Application.Services;
+using Rozpodil.Domain.Entities;
+
+namespace Rozpodil.API.Controllers
+{
+    [Route("api/rooms")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private RoomService _roomService;
+        public RoomController(
+            
+            )
+        {
+
+        }
+        public ActionResult<Room> GetRoomsByUserId(Guid userId)
+        {
+
+        }
+    }
+}
