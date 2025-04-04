ac22880db9db6657c99e6a5b9f05372e3d3e9cf9
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-04 03:16:57 +0300

    feat(backend): create daatabase context

---
+++ backend/Infrastructure/Rozpodil.Persistence/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rozpodil.Domain.Entities;
+
+namespace Rozpodil.Persistence
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseContext(
+            DbContextOptions<DatabaseContext> options
+        ) : base(options) { }
+    }
+}
