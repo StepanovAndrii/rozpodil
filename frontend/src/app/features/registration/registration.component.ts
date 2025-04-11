422966c0be68d5f89b61549bd3512c6fcfc9272a
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-11 10:56:40 +0300

    feat(registration): create registration form group

--- frontend/src/app/features/registration/registration.component.ts
+++ frontend/src/app/features/registration/registration.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { InputFieldComponent } from "../../core/components/input-field/input-field.component";
 import { PasswordFieldComponent } from "../../core/components/password-field/password-field.component";
 
 @Component({
   selector: 'app-registration',
-  imports: [RouterLink, InputFieldComponent, PasswordFieldComponent],
+  imports: [
+    RouterLink,
+    InputFieldComponent,
+    PasswordFieldComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
@@ -13,12 +19,22 @@ import { PasswordFieldComponent } from "../../core/components/password-field/pas
 export class RegistrationComponent {
   public registerWithGoogleUrl: string = "";
   public loginUrl: string = "/login";
+
+  public registrationForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    passwordRepetition: new FormControl('')
+  });
   
   public constructor(
     private router: Router
   ) { }
 
   public changeToLogin() {
-    this.router.navigate(['/login'], { replaceUrl: true });
+    this.router.navigate(
+      ['/login'],
+      { replaceUrl: true }
+    );
   }
 }
