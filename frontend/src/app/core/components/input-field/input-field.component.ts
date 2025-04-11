2a0c7159727bd2d4e574007769b940cd8c7d2a63
Stepanov Andrii <stepanovandriimail@gmail.com>
2025-04-11 10:54:55 +0300

    feat(input-field): implement ControlValueAccessor interface in InputFieldComponent

--- frontend/src/app/core/components/input-field/input-field.component.ts
+++ frontend/src/app/core/components/input-field/input-field.component.ts
@@ -1,13 +1,65 @@
-import { Component, Input } from '@angular/core';
+import { 
+  ChangeDetectionStrategy,
+  Component,
+  forwardRef,
+  Input,
+  ChangeDetectorRef
+} from '@angular/core';
+
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR
+} from '@angular/forms';
+
+import { OnChangeFn } from '../types/on-change-fn.types';
+import { OnTouchedFn } from '../types/on-touched-fn.types';
 
 @Component({
   selector: 'app-input-field',
   imports: [],
   standalone: true,
   templateUrl: './input-field.component.html',
-  styleUrl: './input-field.component.scss'
+  styleUrl: './input-field.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(
+      () => InputFieldComponent
+    ),
+    multi: true
+  }]
 })
-export class InputFieldComponent {
+
+export class InputFieldComponent implements ControlValueAccessor {
   @Input() inputFieldId!: string;
   @Input() inputType: string = 'text';
+  @Input() value: string = '';
+
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  public constructor(
+    private readonly changeDetector: ChangeDetectorRef
+  ) { }
+
+  public onInputValueChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value: string = target.value;
+    this.onChange(value);
+  }
+
+  writeValue(value: string): void {
+    if(value === null) return;
+    this.value = value;
+    this.changeDetector.detectChanges();
+  }
+
+  registerOnChange(fn: OnChangeFn): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: OnTouchedFn): void {
+    this.onTouched = fn;
+  }
 }
+
